b9a4862cb6b391529639d97fb144925a73a1a38a
Joe Gowen <jadgowen@gmail.com>
2018-12-03 20:09:26 -0700

    Added Switch to handle 404 errors

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Search from './views/Search'
 import Home from './views/Home'
-import { Route } from 'react-router-dom'
+import CatchAll from './components/Catch-All'
+import { Route, Switch } from 'react-router-dom'
 
 class BooksApp extends React.Component {
   //Empty array to store book objects
@@ -37,20 +38,24 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
-        {/*Displays Home view and passes books state and moveShelf function*/}
-        <Route exact path={"/"} render={() => (
-          <Home
-            books={this.state.books}
-            moveShelf={this.moveShelf}
-          />
-        )}/>
-        {/*Displays Search view and passes books state and moveShelf function*/}
-        <Route exact path={"/search"} render={() => (
-          <Search
-            books={this.state.books}
-            moveShelf={this.moveShelf}
-          />
-        )}/>
+        {/*Added Switch to handle 404 errors per reviewer suggestion*/}
+        <Switch>
+          {/*Displays Home view and passes books state and moveShelf function*/}
+          <Route exact path={"/"} render={() => (
+            <Home
+              books={this.state.books}
+              moveShelf={this.moveShelf}
+            />
+          )}/>
+          {/*Displays Search view and passes books state and moveShelf function*/}
+          <Route exact path={"/search"} render={() => (
+            <Search
+              books={this.state.books}
+              moveShelf={this.moveShelf}
+            />
+          )}/>
+          <Route component={CatchAll}/>
+        </Switch>
       </div>
     )
   }
---
+++ src/components/Catch-All.js
@@ -0,0 +1,9 @@
+import React, { Component} from 'react'
+
+class CatchAll extends Component {
+  render() {
+    return (<div>Sorry! Your URL does not exist!</div>)
+  }
+}
+
+export default CatchAll
